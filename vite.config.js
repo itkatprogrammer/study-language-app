92b3b8a57a8b5195e936365fdb07d96368da0fc2
Ekaterina Antsiferova <itkatprogrammer@gmail.com>
2024-04-08 02:56:13 +0400

    configs changes final

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';   
-import VitePluginGhPages from 'vite-plugin-gh-pages';
-   
-   export default defineConfig({
-       plugins: [
-           new VitePluginGhPages()
-       ]
-   });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
