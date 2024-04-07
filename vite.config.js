e50803ec93d195f979d0b4f4a6ebadd30c9ad686
Ekaterina Antsiferova <itkatprogrammer@gmail.com>
2024-04-08 02:36:25 +0400

    config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite';   
 import VitePluginGhPages from 'vite-plugin-gh-pages';
-
-
-   import VitePluginGhPages from 'vite-plugin-gh-pages';
    
    export default defineConfig({
        plugins: [
